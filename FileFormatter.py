15f7df2b71c9ae78237dea1f2568d7e3fbd6aae8
atulsaurav <atulsaurav@users.noreply.github.com>
2014-04-06 17:03:13 -0500

    Create FileFormatter.py

---
+++ FileFormatter.py
@@ -0,0 +1,7 @@
+from xml.dom import minidom
+
+# this script reads an informatica mapping xml as an input and formats a source or 
+# target file based on a source or target qualifier definition in the mapping. The
+# file is formatted with a field delimiter so it can easily be imported in excel for
+# analysis
+
